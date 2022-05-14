ac9d2c67b88a65173c8313ffcf80e88531f488d1
Michael Keller <13169777+Michaelvsk@users.noreply.github.com>
2022-05-14 12:00:44 +0200

    feat: extend model

--- Models/GameItem.cs
+++ Models/GameItem.cs
@@ -1,14 +1,16 @@
 ﻿namespace Models;
 
-internal class GameItem : BaseItem
+public class GameItem : BaseItem
 {
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
 
     public string? Description { get; set; }
 
-    public Guid Cover { get; set; }
+    public Guid Cover { get; set; } = Guid.Empty;
 
     public HashSet<Platform> Platform { get; set; } = new HashSet<Platform>();
     
     public HashSet<Genre> Genre { get; set; } = new HashSet<Genre>();
+
+    public Rating Rating { get; set; } = Rating.NotRated;
 }
---
+++ Models/Rating.cs
@@ -0,0 +1,9 @@
+﻿namespace Models;
+public enum Rating
+{
+    NotRated = -1,
+    Bad = 0,
+    BelowAverage = 1,
+    AboveAverage = 2,
+    Top = 3
+}
