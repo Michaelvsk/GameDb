aa8db2795345c4c16d954818ae48dc6de7bb4202
Michael Keller <13169777+Michaelvsk@users.noreply.github.com>
2022-06-26 15:35:01 +0200

    feat: add Cover model class

---
+++ GameDb.Models/Cover.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Diagnostics;
+
+namespace Michaelvsk.GameDb.Models;
+public class Cover : BaseItem
+{
+    byte[] _pngData;
+    public byte[] PngData
+    {
+        get => _pngData;
+        
+        set
+        {
+            Guard.IsNotNull(value);
+            Guard.IsGreaterThan(value.Length, 0);
+
+            _pngData = value;
+        }
+    }
+
+    // Constructor indeed ensures that _pngData is properly initialized. Check are in PngData's setter.
+    #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Cover(byte[] pngData)
+    #pragma warning restore CS8618
+    {
+        PngData = pngData;
+    }
+}
